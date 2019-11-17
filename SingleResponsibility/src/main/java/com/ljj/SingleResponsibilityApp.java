721145fa9e94391a9365da366e53f2e3edb56d6a
kanateSong <545430154@qq.com>
2019-11-17 15:29:00 +0800

    单一责任原则

---
+++ SingleResponsibility/src/main/java/com/ljj/SingleResponsibilityApp.java
@@ -0,0 +1,13 @@
+package com.ljj;
+
+/**
+ * Hello world!
+ *
+ */
+public class SingleResponsibilityApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
